7021a0106a54ff051ec6d26b3dcf573500a04f59
Young Hyun <yooyh939@gmail.com>
2017-03-29 10:39:45 +0000

    menusite!

---
+++ app/controllers/food_controller.rb
@@ -0,0 +1,12 @@
+class FoodController < ApplicationController
+    def menu
+        @foodname = ["치킨","피자","부대찌개"]
+        @pick = @foodname.sample
+        @image = @pick + ".jpg"
+        
+        
+    end
+
+
+
+end
